5fe78a6cdb5afe2a795ed41c5a17a7be31266b33
tiszaszaki <jobljr9717@gmail.com>
2022-05-29 23:09:40 +0200

    added CORS

--- ff-todo-aspnet/Program.cs
+++ ff-todo-aspnet/Program.cs
@@ -20,9 +20,18 @@ builder.Services.AddScoped<TaskService>();
 
 builder.Services.AddControllers();
 
+//services cors
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder => {
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+//app cors
+app.UseCors("corsapp");
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
