58e8fb728c73918eace1ad7123ecb6d1ecaee3e3
tiszaszaki <jobljr9717@gmail.com>
2022-07-06 19:50:23 +0200

    Some fixes/features - #92 [ASP.NET] [For testing] Change log verbosity: show messages only with level between Warning and Error - #71 [ASP.NET] Integration tests - BoardIntegrationTests - GetBoardIds()

---
+++ ff-todo-aspnet-test/BoardIntegrationTest.cs
@@ -0,0 +1,30 @@
+﻿using ff_todo_aspnet.Constants;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json.Linq;
+using Xunit.Abstractions;
+
+namespace ff_todo_aspnet_test;
+public class BoardIntegrationTest
+{
+    private readonly HttpClient client;
+    private readonly ITestOutputHelper logger;
+
+    public BoardIntegrationTest(ITestOutputHelper logger)
+    {
+        var webapp = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder => builder.UseSetting("IsRealDatabase", "false"));
+        client = webapp.CreateDefaultClient();
+        this.logger = logger;
+    }
+
+    [Fact]
+    public async Task GetBoardIds()
+    {
+        var response = await client.GetAsync(TodoCommon.boardPath);
+        response.EnsureSuccessStatusCode();
+        var responseObject = JArray.Parse(await response.Content.ReadAsStringAsync());
+        var expectedObject = new JArray();
+        logger.WriteLine($"GetBoardIds: {expectedObject},{responseObject}");
+        Assert.Equal(expectedObject, responseObject);
+    }
+}
